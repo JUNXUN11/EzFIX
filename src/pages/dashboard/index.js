12f8aa8553a8db8d3748ed174352f7111564f347
Jiayuuuuuuuuu <preveenong@gmail.com>
2024-11-08 22:49:29 +0800

    Create Report Page

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -3,4 +3,4 @@ export * from "@/pages/dashboard/profile";
 export * from "@/pages/dashboard/report";
 export * from "@/pages/dashboard/report-admin";
 export * from "@/pages/dashboard/notifications";
-export * from "@/pages/dashboard/reports";
+export * from "@/pages/dashboard/CreateReport";
