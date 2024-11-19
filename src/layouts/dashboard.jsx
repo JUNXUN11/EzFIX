7e94845146567e36794b6796ba8b83599cf7af0b
Jiayuuuuuuuuu <preveenong@gmail.com>
2024-11-19 13:53:11 +0800

    Place a logo in SideNav Bar

--- src/layouts/dashboard.jsx
+++ src/layouts/dashboard.jsx
@@ -12,14 +12,13 @@ import { useMaterialTailwindController, setOpenConfigurator } from "@/context";
 
 export function Dashboard() {
   const [controller, dispatch] = useMaterialTailwindController();
-  const { sidenavType } = controller;
 
   return (
     <div className="min-h-screen bg-blue-gray-50/50">
       <Sidenav
         routes={routes}
         brandImg={
-          sidenavType === "dark" ? "/img/logo-ct.png" : "/img/logo-ct-dark.png"
+          "/img/ezfixlogo.png" 
         }
       />
       <div className="p-4 xl:ml-80">
