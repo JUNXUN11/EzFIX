64f469bbaf63c27d4d30f441205430c6e7e333d5
Xinyaaaa <siowxinya99@gmail.com>
2024-12-14 14:45:58 +0800

    latest

--- src/data/statistics-cards-data.js
+++ src/data/statistics-cards-data.js
@@ -51,7 +51,7 @@ export function useStatisticsCardsData() {
 
         setCardsData([
           {
-            color: "orange",
+            color: "yellow",
             icon: ClipboardDocumentListIcon,
             title: "New Report",
             value: newReports.toString(),
@@ -69,9 +69,9 @@ export function useStatisticsCardsData() {
             value: rejectedReports.toString(),
           },
           {
-            color: "yellow",
+            color: "orange",
             icon: ExclamationCircleIcon,
-            title: "Pending Reports",
+            title: "In Progress Reports",
             value: pendingReports.toString(),
           },
         ]);
